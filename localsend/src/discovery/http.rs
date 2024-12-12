24b0196b1fc43a01bdb3a406f0f0650bc57ce611
wylited <44154103+wylited@users.noreply.github.com>
2024-12-12 22:17:54 +0800

    legacy discover

--- localsend/src/discovery/http.rs
+++ localsend/src/discovery/http.rs
@@ -15,6 +15,16 @@ impl Client {
         }
         Ok(())
     }
+
+    pub async fn announce_http_legacy(&self, address_list: Vec<SocketAddrV4>) -> crate::error::Result<()> {
+        let client = reqwest::Client::new();
+        for ip in address_list {
+            let url = format!("http://{}/api/localsend/v2/register", ip);
+            let res = client.post(&url).json(&self.device).send().await?;
+            println!("{:?}", res);
+        }
+        Ok(())
+    }
 }
 
 pub async fn register_device(
